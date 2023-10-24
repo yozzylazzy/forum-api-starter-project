b5e5e9defebc650049d27292713531c0b0e49fa9
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-10-24 10:28:33 +0700

    refactor and fix missing test on use_case

--- src/Applications/use_case/_test/DeleteCommentUseCase.test.js
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -3,6 +3,30 @@ const CommentRepository = require('../../../Domains/comments/CommentRepository')
 const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
 
 describe('DeleteCommentUseCase', () => {
+  it('should throw error if use case payload not meet data type specification', async () => {
+    // Arrange
+    const useCasePayload = {
+      threadId: 123,
+      owner: { id: 'user-123' },
+      commentId: 12.3,
+    };
+    const deleteCommentUseCase = new DeleteCommentUseCase({});
+    // Action & Assert
+    await expect(deleteCommentUseCase.execute(useCasePayload))
+      .rejects
+      .toThrowError('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+  it('should throw error if payload not contain needed payload', async () => {
+    // Arrange
+    const useCasePayload = {
+      owner: 'user-123'
+    };
+    const deleteCommentUseCase = new DeleteCommentUseCase(useCasePayload);
+    // Action & Assert
+    await expect(deleteCommentUseCase.execute(useCasePayload))
+      .rejects
+      .toThrowError('DELETE_COMMENT_USE_CASE.NOT_CONTAIN_NEEDED_PAYLOAD');
+  });
   it('should orchestrating the delete comment action correctly', async () => {
     // Arrange
     const useCasePayload = {
@@ -30,9 +54,13 @@ describe('DeleteCommentUseCase', () => {
     // Action
     await deleteCommentUseCase.execute(useCasePayload);
     // Assert
-    expect(mockThreadRepository.verifyThreadExist).toBeCalledWith(useCasePayload.threadId);
-    expect(mockCommentRepository.verifyCommentExist).toBeCalledWith(useCasePayload.commentId);
-    expect(mockCommentRepository.verifyCommentOwner).toBeCalledWith(useCasePayload.commentId, useCasePayload.owner);
-    expect(mockCommentRepository.deleteCommentById).toBeCalledWith(useCasePayload.commentId);
+    expect(mockThreadRepository.verifyThreadExist)
+      .toBeCalledWith(useCasePayload.threadId);
+    expect(mockCommentRepository.verifyCommentExist)
+      .toBeCalledWith(useCasePayload.commentId);
+    expect(mockCommentRepository.verifyCommentOwner)
+      .toBeCalledWith(useCasePayload.commentId, useCasePayload.owner);
+    expect(mockCommentRepository.deleteCommentById)
+      .toBeCalledWith(useCasePayload.commentId);
   });
 });
