2369659f6fbed11645e3aec63b6e007325945c57
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-10-24 09:34:54 +0700

    implement replies at show threads

--- src/Applications/use_case/ShowThreadUseCase.js
+++ src/Applications/use_case/ShowThreadUseCase.js
@@ -1,17 +1,21 @@
 class ShowThreadUseCase {
   constructor({
-    threadRepository, commentRepository,
+    threadRepository, commentRepository, replyRepository
   }) {
     this._threadRepository = threadRepository;
     this._commentRepository = commentRepository;
+    this._replyRepository = replyRepository;
   }
   async execute(useCasePayload) {
     const thread = await this._threadRepository.getThreadById(useCasePayload);
     const comments = await this._commentRepository.getCommentsByThreadId(useCasePayload);
+    const replies = await this._replyRepository.getRepliesByThreadId(useCasePayload);
     const validatedComments = this._validateDeletedComment(comments);
+    const validatedReplies = this._validateDeletedReply(replies);
+    const commentsWithReplies = this._addReplyToComment(validatedComments, validatedReplies);
     return {
       ...thread,
-      comments: validatedComments,
+      comments: commentsWithReplies,
     };
   }
   _validateDeletedComment(comments) {
@@ -20,6 +24,27 @@ class ShowThreadUseCase {
         comment.content = '**komentar telah dihapus**';
       }
       delete comment.is_delete;
+    }
+    return comments;
+  }
+  _validateDeletedReply(replies) {
+    for (const reply of replies) {
+      if (reply.is_delete) {
+        reply.content = '**balasan telah dihapus**';
+      }
+      delete reply.is_delete;
+    }
+    return replies;
+  }
+  _addReplyToComment(comments, replies) {
+    for (const comment of comments) {
+      comment.replies = [];
+      for (const reply of replies) {
+        if (reply.comment_id === comment.id) {
+          comment.replies.push(reply);
+        }
+        delete reply.comment_id;
+      }
     }
     return comments;
   }
