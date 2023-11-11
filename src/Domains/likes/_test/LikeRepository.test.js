186be4f4524e8f01fdf43a12ebab286a37137202
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-11-11 14:55:18 +0700

    Add LikeRepository test case Domain Level

---
+++ src/Domains/likes/_test/LikeRepository.test.js
@@ -0,0 +1,13 @@
+const LikeRepository = require('../LikeRepository');
+
+describe('', () => {
+  it('should throw error when invoke abstract behaviour', async () => {
+    // Arrange
+    const likeRepository = new LikeRepository();
+    // Action & Assert
+    await expect(likeRepository.createLike({})).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.verifyIsLiked('')).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.cancelLike('')).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.getLikesCountByComments('')).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
